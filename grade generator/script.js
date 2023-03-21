1254691a52f8d59311e20cc3807694905761c8f1
henry cheche <henry.cheche@student.moringaschool.com>
2023-03-21 17:43:19 +0300

    Attempt 2

--- grade generator/script.js
+++ grade generator/script.js
@@ -1,35 +1,41 @@
-function calculate(){
-    //link inputed values to javascript
-    let math = document.getElementById(`math`).value
-    let eng = document.getElementById(`eng`).value
-    let swa = document.getElementById(`swa`).value
-    let phy = document.getElementById(`phy`).value
-    let chem = document.getElementById(`chem`).value
+function calculate() {
+  //link inputed values to javascript
+  let math = document.getElementById(`math`).value;
+  let eng = document.getElementById(`eng`).value;
+  let swa = document.getElementById(`swa`).value;
+  let phy = document.getElementById(`phy`).value;
+  let chem = document.getElementById(`chem`).value;
+  // alert if input fields are empty
+  if (math == "" || eng == "" || swa == "" || phy == "" || chem == "") {
+    alert(`Please fill all input fields`);
+  } else {
     let grade;
     //calculate total marks
-    const total = parseFloat(math) +parseFloat(eng)+parseFloat(swa)+parseFloat(phy)+parseFloat(chem);
+    const total =
+      parseFloat(math) +
+      parseFloat(eng) +
+      parseFloat(swa) +
+      parseFloat(phy) +
+      parseFloat(chem);
     //calculate percentage
-    const per = (total/500)*100;
+    const per = (total / 500) * 100;
     //display grade depending on percentage
-    if (per >79 && per <=100){
-      grade = `A`
-    }
-    else if (per >=60 && per <80){
-      grade = `B`
-    }
-    else if (per >=50 && per <60){
-      grade = `C`
-    }
-    else if (per >=40 && per <50){
-      grade = `D`
-    }
-    else if (per >=0 && per <40){
-      grade = `E`
+    if (per > 79 && per <= 100) {
+      grade = `A`;
+    } else if (per >= 60 && per < 80) {
+      grade = `B`;
+    } else if (per >= 50 && per < 60) {
+      grade = `C`;
+    } else if (per >= 40 && per < 50) {
+      grade = `D`;
+    } else if (per >= 0 && per < 40) {
+      grade = `E`;
     }
     //link javascript to paragraphs in index.html
-    document.getElementById(`result`).innerHTML= 
-    `Your total marks are: ${total}`
-    document.getElementById(`per`).innerHTML=`Your percentage is: ${per}%`
-    document.getElementById(`grade`).innerHTML=`Your grade is: ${grade} `
-    
+    document.getElementById(
+      `result`
+    ).innerHTML = `Your total marks are: ${total}`;
+    document.getElementById(`per`).innerHTML = `Your percentage is: ${per}%`;
+    document.getElementById(`grade`).innerHTML = `Your grade is: ${grade} `;
+  }
 }
--- speed detector/script.js
+++ speed detector/script.js
@@ -1,25 +1,27 @@
-//link input to javascript
-const enterString = document.getElementById (`enterString`)
-const result = document.getElementById (`result`)
-        
-function calculate(){
+function calculate() {
+  //link input to javascript
+  let enterString = document.getElementById(`enterString`).value;
+  let result = document.getElementById(`result`).value;
+
+  if (enterString == "") {
+    alert(`Please fill the field below`);
+  } else {
     //convert value to integer
-    const enter = parseInt(enterString.value)
+    const enter = parseInt(enterString);
     //calculate demerit points
     let point;
-    point = eval((enter-70)/5)
+    point = eval((enter - 70) / 5);
     //give response if speed is above limit
     let answer;
-    if(enter <=70){
-        answer=`OK`
+    if (enter <= 70) {
+      answer = `OK`;
+    } else if (enter > 70 && point < 12) {
+      answer = `This is a warning`;
+    } else if (enter > 70 && point >= 12) {
+      answer = `License suspended`;
     }
-    else if(enter > 70 && point<12){
-        answer= `This is a warning`
-    }
-    else if (enter >70 && point >=12){
-        answer = `License suspended`
-    }
-    result.innerText= `${answer}. Your current demerit points are: ${point}`
-
-}       
-        
+    document.querySelector(
+      `#result`
+    ).innerText = `${answer}. Your current demerit points are: ${point}`;
+  }
+}
